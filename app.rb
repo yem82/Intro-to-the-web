0d0c4f75bcde421c25e44a19389f8378880573d8
Yemi <yemiadebayo60@gmail.com>
2019-05-14 16:05:54 +0100

    Use POST request

--- app.rb
+++ app.rb
@@ -17,9 +17,23 @@ get "/random-cat" do
   erb(:index)
 end
 
-get "/named-cat" do
+post "/named-cat" do
   p params
   @name = params[:name]
   @age = params[:age]
   erb(:index)
 end
+
+get "/cat-form" do
+  erb(:cat_form)
+end
+
+# Some interesting things have happened as a result of changing our GET request to a POST request.
+#
+# First of all, the query string on the /named-cat page has disappeared.
+# This is because POST requests do not store their query parameters in the request string.
+# Instead, they store them in the body of the request as 'Form Data'.
+#
+# Secondly, the params we print to the console have not changed.
+# This is what we expect: although POST sends parameters from clients to servers in a different way to GET,
+# it still sends them, and they are interpreted the same way by Sinatra.
